92ac4c56c5c2653f4db981c92686ac3ccc596142
Kenny Peel <knowonehome@gmail.com>
2022-10-06 19:40:48 -0500

    Update Configuration.h
    moved bltouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1174,7 +1174,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 63, -35, -2.30 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -35, -2.30 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
