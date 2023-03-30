ab733106af2447226e57378e6e365b49ccea03f0
knowonehome <knowonehome@gmail.com>
2023-03-30 10:14:59 -0500

    Update Configuration.h
    Updated nozzel to prope offset and max z position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1174,7 +1174,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -20, -47, -2.30 }
+#define NOZZLE_TO_PROBE_OFFSET { -22, -43, -2.80 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1373,7 +1373,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 380
+#define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
